0dcc2e193464a87760e61bbfe43ce8875ec55d2e
Pavitra Golchha <hello@pavi2410.me>
2025-04-13 21:27:30 +0530

    fix

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,48 +1,22 @@
 // app/routes/index.tsx
-import * as fs from 'node:fs'
-import { createFileRoute, useRouter } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
+import { createFileRoute } from '@tanstack/react-router'
 import { Button } from '@/components/ui/button'
-
-const filePath = 'count.txt'
-
-async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-  )
-}
-
-const getCount = createServerFn({
-  method: 'GET',
-}).handler(() => {
-  return readCount()
-})
-
-const updateCount = createServerFn({ method: 'POST' })
-  .validator((d: number) => d)
-  .handler(async ({ data }) => {
-    const count = await readCount()
-    await fs.promises.writeFile(filePath, `${count + data}`)
-  })
+import React from 'react'
 
 export const Route = createFileRoute('/')({
   component: Home,
-  loader: async () => await getCount(),
 })
 
 function Home() {
-  const router = useRouter()
-  const state = Route.useLoaderData()
+  const [count, setCount] = React.useState(0)
 
   return (
     <Button
       onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate()
-        })
+        setCount(count + 1)
       }}
     >
-      Add 1 to {state}?
+      Add 1 to {count}?
     </Button>
   )
 }
