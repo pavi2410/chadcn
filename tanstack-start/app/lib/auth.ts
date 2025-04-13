d627f999efc6cb35e6b26f7980ab49937364ec04
Pavitra Golchha <hello@pavi2410.me>
2025-04-13 21:09:57 +0530

    better-auth

---
+++ tanstack-start/app/lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { reactStartCookies } from "better-auth/react-start";
+import { db } from "@/db";
+import { user, session, account, verification } from "@/db/auth-schema";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "sqlite",
+    schema: {
+      user,
+      session,
+      account,
+      verification,
+    }
+  }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!
+    }
+  },
+  plugins: [reactStartCookies()] // make sure this is the last plugin in the array
+});
